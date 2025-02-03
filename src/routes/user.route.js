b63ef68741eb4af0ffe78c8f9de08554e5079065
ayusclg <freelancerayus@gmail.com>
2025-02-03 21:02:32 +0545

    route added

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,10 +1,12 @@
 import Router from "express";
 import { Upload } from "../middleware/multer.middleware.js";
-import { userLogin, userRegister } from "../controllers/user.controller.js";
+import { userLogin, userLogout, userRegister } from "../controllers/user.controller.js";
+import { VerifyToken } from "../middleware/auth.middleware.js";
 
 const router = Router()
 
 router.route("/register").post(Upload.single('avatar'),userRegister)
 router.route("/login").post(userLogin)
+router.route("/logout").post(VerifyToken,userLogout)
 
 export default router
