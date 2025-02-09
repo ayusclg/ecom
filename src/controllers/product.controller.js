8ade0ab292162f5f581b01d2e408e58505cac025
ayusclg <freelancerayus@gmail.com>
2025-02-09 20:22:19 +0545

    Product CREATE & Fetch

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -1,17 +1,21 @@
 import {Product} from '../models/product.models.js'
+import { User } from '../models/user.models.js'
 const addProduct = async(req,res)=>{
     try {
         const{title,description,price,category,in_stock}= req.body
-
-        imagePath = req.file?`public/images/${req.file.filename}`:"";
-        const admin = await Product.findById(req.user._id)
-
+        
+         const imagePath =`public/images/${req.file.filename}`
+         
+        
+    
+        const admin = await User.findById(req.user._id)
+        
         if(!admin.isAdmin){
             return res.status(401).json({
-                message:"you are admin"
+                message:"you are not admin"
             })
         }
-
+        
         const productCreated = await Product.create({
             title,
             description,
@@ -20,24 +24,55 @@ const addProduct = async(req,res)=>{
             in_stock,
             image:imagePath
         })
+        
         if (!productCreated) {
             return res.status(500).json({
               message: 'Something went wrong'
             })
           }
       
-
+          
         return res.status(200).json({
             message: "Product added",
-            data:productCreated
+            data: productCreated
         })
 
 
     } catch (error) {
-        return res.status(500).json({
-            message:"Product couldnot register"
+         res.status(500).json({
+            message:"Product couldnot register",
+            message: error
         })
     }
 }
+ const fetchProduct = async (req,res)=>{
+    try {
+        let page = parseInt(req.query.page) || 1
+        let perPage = parseInt(req.query.perPage)|| 2
+        let category = req.query.category
 
-export {addProduct}
+        let productFilter = {}
+        if(category){
+            productFilter.categories = category
+        }
+
+        const product = await Product.find(productFilter)
+        .limit(perPage)
+        .skip((page-1)*perPage)
+
+        const totalProduct = await Product.countDocuments(productFilter)
+
+            res.status(201).json({
+                page:page,
+                perPage,
+                totalProducts : totalProduct,
+                data : product
+            }
+        )
+    } catch (error) {
+        res.status(500).json({
+            message: "Error occured in fetching products"
+        })
+    }
+ }
+export {addProduct,fetchProduct}
--- src/routes/product.route.js
+++ src/routes/product.route.js
@@ -1,10 +1,11 @@
 import Router from "router";
 import { VerifyToken } from "../middleware/auth.middleware.js";
-import { addProduct } from "../controllers/product.controller.js";
+import { addProduct, fetchProduct } from "../controllers/product.controller.js";
 import { Upload } from "../middleware/multer.middleware.js";
 
 const router = Router()
 
 router.route("/addProduct").post(Upload.single("image"),VerifyToken,addProduct)
+router.route("/fetch").get(fetchProduct)
 
 export default router
